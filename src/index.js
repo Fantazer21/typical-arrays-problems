9f8b2cd5b7b6abc0e1c724524c0789b500c901eb
Ilya21 <6967221@gmail.com>
2020-09-30 21:25:53 +0200

    feat : Final decision for max,min,av value

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
 exports.min = function min (array) {
-  return 0;
+if (array == undefined|| array.length === 0) {
+	return 0;
+}else { 
+		var minimum = array[0]
+		for (var i = 0; i <= array.length; i++) {
+  		if (array[i] <= minimum) {
+  		minimum = array[i];
 }
+}return minimum;
+}
+}
+
+
+
+  
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined|| array.length === 0) {
+return 0; 
+}else { 
+		var maximum = array[0]
+		for (var i = 0; i <= array.length; i++) {
+  		if (array[i] >= maximum) {
+  		maximum = array[i];
+}
+}return maximum;
+}
 }
 
+
+
 exports.avg = function avg (array) {
-  return 0;
+ if (array == undefined|| array.length === 0) {
+return 0; 
+} else {
+  			var average  = 0;
+  			var sum = 0; 
+   		for (var i = 0; i < array.length; i++) {
+   		sum += array[i];
+   		} average = sum/(array.length);
+  		 return average;
+  }
 }
